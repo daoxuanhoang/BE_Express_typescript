384a50876d9371241f81d8a4c3342231c63a902a
vinpro <hienvinh24@gmail.com>
2022-11-18 21:21:00 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import app from './app'
+import mongoose from 'mongoose'
+import { config } from './config/config'
+import Logging from './libraries/Logging'
+
+/** Connect to Mongo */
+mongoose
+    .connect(config.mongoUrl, { retryWrites: true, w: 'majority' })
+    .then(() => {
+        Logging.info('Mongo connected successfully.')
+        StartServer()
+    })
+    .catch((error) => Logging.error(error))
+
+/** Only Start Server if Mongoose Connects */
+const StartServer = async () => {
+    Logging.info('Server is checking workflow for merge...')
+
+    /** Throw error if config variable undefine */
+    if (!config.mongoUrl) {
+        throw new Error('JWT_KEY must be defined')
+    }
+
+    try {
+        await mongoose.connect(config.mongoUrl, { retryWrites: true, w: 'majority' })
+        Logging.success('Mongo connected successfully.')
+
+        Logging.info('Server has started!!!')
+        app.listen(config.server.port, () => Logging.success(`Server is running on port ${config.server.port}`))
+    } catch (error) {
+        Logging.error(error)
+    }
+}
